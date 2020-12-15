f26253b263efe2822a2706bcc923f34f2ef9fee4
Lemuel Boyce <lemuelboyce@gmail.com>
2020-12-14 20:48:16 -0500

    add support for providers specified in environment variables
    Yamz CLI now looks for `YAMZ_DEFAULT_PROVIDER` and uses it when available. This allows you to provide overrides on a per project bases.
    
    In an `.env` file with `YAMZ_DEFAULT_PROVIDER=path.to.Provider`:
    `pipenv run yamz read --key=mykey --env=prod`
    
    Or inline:
    `YAMZ_DEFAULT_PROVIDER=path.to.Provider yamz read --key=mykey --env=prov`

--- yamz/cli.py
+++ yamz/cli.py
@@ -1,31 +1,20 @@
-import importlib
-from typing import Type
+import os
 
 from yamz import Yamz
 from yamz.errors import YamzEnvironmentError
 from yamz.logger import logger
-from yamz.providers.base import BaseProvider
-
-
-def import_resolver(module_path: str) -> Type[BaseProvider]:
-    """
-    Takes a string reference to a class and returns
-    an actual Python class.
-    """
-
-    split_path = module_path.split('.')
-    klass = split_path[-1]
-    package = '.'.join(split_path[:-1])
-    module = importlib.import_module(package)
-    provider_class = getattr(module, klass)
-    assert issubclass(provider_class, BaseProvider)
-    return provider_class
+from yamz.utils import safe_import_resolver
 
 
 def main(action: str, key: str, data: str, environment: str, path: str = None, provider: str = None):
     config = {'path': path}
-    if provider:
-        provider_class = import_resolver(provider)
+
+    provider_ref = provider or os.getenv('YAMZ_DEFAULT_PROVIDER')
+    provider_class = None
+    if provider_ref:
+        provider_class = safe_import_resolver(provider_ref)
+
+    if provider_class:
         config.update({'provider': provider_class})
 
     yamz = Yamz(**config)
--- yamz/logger.py
+++ yamz/logger.py
@@ -1,6 +1,6 @@
 import logging
 
-FORMAT = '%(name)s - %(message)s'
+FORMAT = '%(name)s (%(levelname)s): %(message)s'
 
 logging.basicConfig(format=FORMAT)
 
